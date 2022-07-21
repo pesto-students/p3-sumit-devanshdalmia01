9006b87560c9419233e91f05da86f5c327b1b78e
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-07-21 19:08:14 +0530

    2nd done

--- Week-6/Exercise_6.1/script.js
+++ Week-6/Exercise_6.1/script.js
@@ -1,42 +1,42 @@
 const maxSubArray = (nums) => {
 	// O(n[cube]) Solution
-    // let maxSum = Number.NEGATIVE_INFINITY;
-    // if (nums.length == 1)
-    // {
-    //     return nums[0];
-    // }
-    // for (let i = 0; i < nums.length; i++)
-    // {
-    //     for (let j = i; j < nums.length; j++)
-    //     {
-    //         let sum = 0;
-    //         for (let k = i; k <= j; k++)
-    //         {
-    //             sum += nums[k];
-    //         }
-    //         maxSum = max(sum, maxSum);
-    //     }
-    // }
-    // return maxSum;
+	// let maxSum = Number.NEGATIVE_INFINITY;
+	// if (nums.length == 1)
+	// {
+	//     return nums[0];
+	// }
+	// for (let i = 0; i < nums.length; i++)
+	// {
+	//     for (let j = i; j < nums.length; j++)
+	//     {
+	//         let sum = 0;
+	//         for (let k = i; k <= j; k++)
+	//         {
+	//             sum += nums[k];
+	//         }
+	//         maxSum = max(sum, maxSum);
+	//     }
+	// }
+	// return maxSum;
 
-    // O(n[square])
-    // let maxSum = Number.NEGATIVE_INFINITY;
-    // if (nums.length == 1)
-    // {
-    //     return nums[0];
-    // }
-    // for (let i = 0; i < nums.length; i++)
-    // {
-    //     let sum = 0;
-    //     for (let j = i; j < nums.length; j++)
-    //     {
-    //         sum += nums[j];
-    //         maxSum = max(sum, maxSum);
-    //     }
-    // }
-    // return maxSum;
+	// O(n[square])
+	// let maxSum = Number.NEGATIVE_INFINITY;
+	// if (nums.length == 1)
+	// {
+	//     return nums[0];
+	// }
+	// for (let i = 0; i < nums.length; i++)
+	// {
+	//     let sum = 0;
+	//     for (let j = i; j < nums.length; j++)
+	//     {
+	//         sum += nums[j];
+	//         maxSum = max(sum, maxSum);
+	//     }
+	// }
+	// return maxSum;
 
-    // O(n) Solution
+	// O(n) Solution
 	let sum = 0;
 	let maxSum = Number.NEGATIVE_INFINITY;
 	if (nums.length === 1) {
