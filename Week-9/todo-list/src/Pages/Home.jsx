6d2553efa0648d121d328c3ab2e8ca0c9f989834
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-08-07 17:27:02 +0530

    made basic layout

--- Week-9/todo-list/src/Pages/Home.jsx
+++ Week-9/todo-list/src/Pages/Home.jsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+import Logo from "../Assets/logo.svg";
+import "./Home.css";
 export default function Home() {
-	return <h1>Hello World</h1>;
+	const [date] = useState(new Date());
+	const [activeTasks] = useState();
+	return (
+		<>
+			<header className="header">
+				<img src={Logo} alt="App Logo" height={75} />
+			</header>
+			<main>
+				<section>
+					<h1>
+						<time dateTime={date.toISOString()}>{date.toLocaleDateString("en-us", { weekday: "long", month: "long", day: "numeric" })}</time>
+						<p>{activeTasks?.length} Active Task</p>
+					</h1>
+					<nav>
+						<button></button>
+						<button></button>
+					</nav>
+				</section>
+				<section>
+					<input type="text" name="" id="" />
+					<button type="submit"></button>
+				</section>
+				<section>
+					<ol>
+						<li></li>
+					</ol>
+				</section>
+			</main>
+			<footer>
+				<h1>&copy; 2022 Devansh Dalmia | All Rights Reserved</h1>
+			</footer>
+		</>
+	);
 }
