98b68ac0290632d34d79d520356ea3c8a7c0e09b
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-08-20 01:06:15 +0530

    trivial changes

--- Week-10/url-shortner/src/Pages/ContactUs.jsx
+++ Week-10/url-shortner/src/Pages/ContactUs.jsx
@@ -1,44 +1,9 @@
 import Base from "../Base";
-import ContactUsSVG from "../Assets/ContactUs.svg";
 
 export default function ContactUs() {
 	return (
 		<Base>
-			<main>
-				<section>
-					<div>
-						<img src={ContactUsSVG} alt="Contact_Us" />
-					</div>
-					<div>
-						<p>Get In Touch!</p>
-						<form>
-							<div>
-								<label>Name</label>
-								<input type="text" placeholder="Name" required />
-							</div>
-							<div>
-								<label>Mobile Number</label>
-								<input type="text" pattern="[6789][0-9]{9}" maxLength="10" placeholder="Mobile Number" required />
-							</div>
-							<div>
-								<label>Email</label>
-								<input type="email" placeholder="Email" required />
-							</div>
-							<div>
-								<label>Subject</label>
-								<input type="text" placeholder="Subject" required />
-							</div>
-							<div>
-								<label>Message</label>
-								<textarea placeholder="Message" required />
-							</div>
-							<div>
-								<button type="submit">Send Message</button>
-							</div>
-						</form>
-					</div>
-				</section>
-			</main>
+			<h1 className="font-bold text-center text-5xl mt-5">Contact Us</h1>
 		</Base>
 	);
 }
--- Week-10/url-shortner/src/Pages/Feedback.jsx
+++ Week-10/url-shortner/src/Pages/Feedback.jsx
@@ -1,30 +1,9 @@
 import Base from "../Base";
-import FeedbackSVG from "../Assets/Feedback.svg";
 
 export default function Feedback() {
 	return (
 		<Base>
-			<main>
-				<section>
-					<div>
-						<img src={FeedbackSVG} alt="Feedback" />
-					</div>
-					<div>
-						<p>
-							Thank you for visiting URL Shortner! Please share your experience with us, it will help us grow. <b>Keep Exploring!</b>
-						</p>
-						<form>
-							<div>
-								<label>Message</label>
-								<textarea placeholder="Your message" required />
-							</div>
-							<div>
-								<button type="submit">Submit</button>
-							</div>
-						</form>
-					</div>
-				</section>
-			</main>
+			<h1 className="font-bold text-center text-5xl mt-5">Feedback</h1>
 		</Base>
 	);
 }
